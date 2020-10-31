d99f2d69ec58f9592ed6b891dc3cb454f2403c74
Luke Thompson <luke@streamjar.tv>
2020-10-31 23:50:01 +0000

    chore(gulp): update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ const iconfont = require('gulp-iconfont');
 const autoprefixer = require('gulp-autoprefixer');
 const consolidate = require('gulp-consolidate');
 const del = require('del');
-const runSequence = require('run-sequence');
 const fontName = 'icon';
 
 gulp.task('fontbuilder', () => gulp.src(['svgs/*.svg'])
 	.pipe(iconfont({ fontName, normalize: true }))
 	.on('glyphs', (glyphs) => {
-		gulp.src('_fontTemplate.scss')
+		gulp.src('scss/_fontTemplate.scss')
 			.pipe(consolidate('lodash', {
 				glyphs,
 				fontName,
@@ -37,6 +36,12 @@ gulp.task('clean', () => del(['./docs']));
 gulp.task('views', () => gulp.src(['src/**/*', 'CNAME'])
 		.pipe(gulp.dest('./docs')));
 
-gulp.task('build', ['clean'], (cb) => {
-	runSequence('fontbuilder', ['styles', 'views'], cb);
-});
+gulp.task('build', gulp.series(
+	'clean',
+	'fontbuilder',
+	gulp.parallel(
+		'styles',
+		'fontbuilder',
+		'views',
+	),
+));
